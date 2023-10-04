d0807e35477eba4962c7394b4f8c52cf77d810c2
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-10-04 23:41:55 +0200

    Complete test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+ const removeFromArray = function (array, ...others) {
+    const newArray = [];
+    array.forEach((items) => {
+        if (!others.includes(items)) {
+            newArray.push(items);
+        }
+    })
+    return newArray;
+ };
 
-};
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
