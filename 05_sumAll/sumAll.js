c0a77c08d6a0efa8986b670cbc4a68b862e17c12
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-10-05 16:15:00 +0200

    Complete test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function (numA,numB) {
+    let totalSum = 0;
+    const range = [];
+    const args = [...arguments];
+    const sortedArgs = args.sort();
+        if ((numA + numB) <= 0 || 
+            !Number.isInteger(numA) || 
+            !Number.isInteger(numB)) {
+            return 'ERROR';
+        }
+        else {
+            for (let i = sortedArgs[0]; i <= sortedArgs[sortedArgs.length -1]; i++) {
+                range.push(i);
+            }
+            for (let i = range[0]; i < range.length; i++) {
+                range.forEach((item) => {
+                totalSum += item;
+                })
+            return totalSum;
+        }
+    }
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
