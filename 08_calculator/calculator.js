185c98163b88bd8bc777071f3d47827f985ce834
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-10-25 22:06:58 +0200

    Completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 += num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce(function(total, value) {
+    return total + value;}, 0)
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((num1, num2) => num1 * num2, 1);
 };
 
-const power = function() {
-	
+const power = function(num, elev) {
+	return num ** elev;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	return !(n > 1) ? 1 : factorial(n - 1) * n;
 };
+
 
 // Do not edit below this line
 module.exports = {
