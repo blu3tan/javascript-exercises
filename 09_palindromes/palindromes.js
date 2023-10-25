3a04500894967422f0b9ee39c462b35625875be7
blu3tan <142602465+blu3tan@users.noreply.github.com>
2023-10-25 23:10:14 +0200

    Completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const normalize = word.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    const reverse = normalize.split('').reverse().join('')
+    return (reverse === normalize) ? true : false;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+ 
